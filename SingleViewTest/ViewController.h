441584421b4283eca93e72709c2405cecb8d7bfa
Mario Abruzzi <abruzzim@framework-solutions.com>
2015-04-05 11:37:03 -0400

    Add Toolbar.

--- SingleViewTest/ViewController.h
+++ SingleViewTest/ViewController.h
@@ -11,8 +11,11 @@
 @interface ViewController : UIViewController
 
 // Add scroll view capability.
+//
 @property (strong,nonatomic) UIScrollView *scrollView;
+
 // Declare variable names for the subviews.
+//
 @property (strong, nonatomic) UIImageView *flowChartImg;
 
 @end
--- SingleViewTest/ViewController.m
+++ SingleViewTest/ViewController.m
@@ -10,6 +10,11 @@
 
 @interface ViewController ()
 
+#define TOOLBAR_HEIGHT 50
+
+//
+@property (strong, nonatomic) UIToolbar *toolBar;
+
 @end
 
 @implementation ViewController
@@ -36,6 +41,18 @@
     //
     self.view.backgroundColor = [UIColor yellowColor];
     
+    // Instantiate a tool bar
+    //
+    self.toolBar = [[UIToolbar alloc] init];
+    //
+    self.toolBar.backgroundColor = [UIColor lightGrayColor];
+    //
+    self.toolBar.barStyle = UIBarStyleBlack;
+    //
+    self.toolBar.frame = CGRectMake(0, self.view.frame.size.height - TOOLBAR_HEIGHT, self.view.frame.size.width, TOOLBAR_HEIGHT);
+    //
+    [self addToolbarItems];
+    
     // Instantiate a scroll view with a frame equal to the bounds.
     //
     self.scrollView = [[UIScrollView alloc] initWithFrame:self.view.bounds];
@@ -46,7 +63,7 @@
     
     // Set the size of the content view.
     //
-    self.scrollView.contentSize = CGSizeMake(768, 4914);
+    self.scrollView.contentSize = CGSizeMake(768, 4914 + TOOLBAR_HEIGHT);
     
     // Create, place and size an image view using @property name.
     //
@@ -64,12 +81,38 @@
     //
     [self.view addSubview:self.scrollView];
     
+    // Add the tool bar to the end of the UIView's list of subview.
+    //
+    [self.view addSubview:self.toolBar];
+    
     
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)addToolbarItems {
+    //
+    UIBarButtonItem *btn1 = [[UIBarButtonItem alloc] initWithTitle:@"BTN1" style:UIBarButtonItemStyleDone target:self action:@selector(doButton1:)];
+    UIBarButtonItem *btn2 = [[UIBarButtonItem alloc] initWithTitle:@"BTN2" style:UIBarButtonItemStyleDone target:self action:@selector(doButton2:)];
+    UIBarButtonItem *btn3 = [[UIBarButtonItem alloc] initWithTitle:@"BTN3" style:UIBarButtonItemStyleDone target:self action:@selector(doButton3:)];
+    UIBarButtonItem *spacer = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:self action:nil];
+    NSArray *barButtonItems = [NSArray arrayWithObjects:spacer, btn1, spacer, btn2, spacer, btn3, spacer, nil];
+    [self.toolBar setItems:barButtonItems];
+}
+
+- (void)doButton1:(UIButton *)sender {
+    NSLog(@"%%ViewController-I-TRACE, -doButton1 called.");
+}
+
+- (void)doButton2:(UIButton *)sender {
+    NSLog(@"%%ViewController-I-TRACE, -doButton2 called.");
+}
+
+- (void)doButton3:(UIButton *)sender {
+    NSLog(@"%%ViewController-I-TRACE, -doButton3 called.");
 }
 
 @end
